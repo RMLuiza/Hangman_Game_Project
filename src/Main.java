e14d70e6490df6909864443fa73e798368722004
Luiza <luiza.mradulescu@yahoo.com>
2023-05-12 20:49:34 +0300

    Added random words

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -12,16 +14,17 @@ public class Main {
         // 6. daca exista litera, inlocuim * de la pozitia literei din cuvant in sirul de stelute
 
         Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("perfume", "transport", "manager", "woman", "proportion",
+                "prestige", "vegetable", "environment", "obligation", "example", "liability"));
+        int index = rnd.nextInt(words.size());
         System.out.println("Welcome to Hangman");
-
-
         System.out.print("Cuvantul este ");
-        String hiddenWord = "avion";
+        String hiddenWord = words.get(index);
         char[] stars = new char[hiddenWord.length()];
 //        for (int i = 0; i < stars.length; i++) {
 //            stars[i] = '*';
 //        }
-
         Arrays.fill(stars, '*');
         System.out.println(String.valueOf(stars));
         while (true) {
@@ -38,7 +41,5 @@ public class Main {
                 break;
             }
         }
-
-
     }
 }
